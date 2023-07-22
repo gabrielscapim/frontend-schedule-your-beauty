8d91fce36ff63357bdda93d208cd3cd58b554892
Gabriel <scapimgabriel@gmail.com>
2023-07-22 09:49:17 -0300

    feat: page beauty make

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import PageMain from './pages/PageMain';
 import PageHairstyle from './pages/PageHairstyle';
+import PageBeautyMake from './pages/PageBeautyMake';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" Component={ PageMain } />
         <Route path="hairstyle" Component={ PageHairstyle } />
+        <Route path="beauty-make" Component={ PageBeautyMake } />
       </Routes>
     </BrowserRouter>
   );
