d7ddc086d0a5519c32f883685a9a9ac10f95cfbb
Gabriel <scapimgabriel@gmail.com>
2023-07-22 09:14:31 -0300

    feat: header click

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 
 function Header() {
+  const navigate = useNavigate();
+
   return (
     <header className={ styles.header }>
-      <div className={ styles['header-container'] }>
+      <button
+        className={ styles['header-container'] }
+        onClick={ () => navigate('/') }
+      >
         <div className={ styles['header-name-principal'] }>
           isabela colli
         </div>
         <div className={ styles['header-name-secondary'] }>
           beauty
         </div>
-      </div>
+      </button>
     </header>
   );
 }
