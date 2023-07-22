c8b09dbf54d8a92f5d56727c6bf4b11077bcb661
Gabriel <scapimgabriel@gmail.com>
2023-07-22 11:56:14 -0300

    style: header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,19 +2,35 @@ import styles from './Header.module.css';
 
 function Header() {
   const navOptionClass = 'nav-option';
+  const navLocationStyle = {
+    fontWeight: 'bold',
+  };
+  const location = window.location.href;
 
   return (
     <header className={ styles.header }>
       <nav>
         <div className={ styles['header-line-1'] }>
           <div className={ styles['header-line-1-nav'] }>
-            <a href="/beauty-make" className={ styles[navOptionClass] }>
+            <a
+              href="/beauty-make"
+              className={ styles[navOptionClass] }
+              style={ location.includes('beauty') ? navLocationStyle : {} }
+            >
               Maquiagem Beauty
             </a>
-            <a href="/special-day-make" className={ styles[navOptionClass] }>
+            <a
+              href="/special-day-make"
+              className={ styles[navOptionClass] }
+              style={ location.includes('special-day') ? navLocationStyle : {} }
+            >
               Maquiagem Special Day
             </a>
-            <a href="/hairstyle" className={ styles[navOptionClass] }>
+            <a
+              href="/hairstyle"
+              className={ styles[navOptionClass] }
+              style={ location.includes('hairstyle') ? navLocationStyle : {} }
+            >
               Penteados
             </a>
           </div>
