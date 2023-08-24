cbba376505e72aabf555bb6925e4a6d0e5fb426d
Gabriel <scapimgabriel@gmail.com>
2023-08-24 18:30:42 -0300

    feat: schedule confirmed component

---
+++ src/components/ScheduleComponents/ScheduleConfirmed.jsx
@@ -0,0 +1,16 @@
+import './ScheduleConfirmed.css';
+
+function ScheduleConfirmed() {
+  return (
+    <div className="schedule-confirmed-container">
+      <i className="bi bi-check-circle"> </i>
+      <h3>Obrigada!</h3>
+      <p>
+        Seu agendamento foi confirmado
+        e entrarei em contato com você em instantes.
+      </p>
+    </div>
+  );
+}
+
+export default ScheduleConfirmed;
--- src/pages/PageSchedule/index.jsx
+++ src/pages/PageSchedule/index.jsx
@@ -12,6 +12,7 @@ import {
   isInputsCorrect } from '../../helpers/verifyScheduleInputs';
 import Modal from '../../components/Modal';
 import whatsAppRequest from '../../services/whatsAppRequest';
+import ScheduleConfirmed from '../../components/ScheduleComponents/ScheduleConfirmed';
 
 function PageSchedule() {
   const [state, setState] = useState({
@@ -24,7 +25,8 @@ function PageSchedule() {
     eventHour: '',
   });
   const [inputWarningShouldAppear, setInputWarningShouldAppear] = useState(false);
-  const [modalOpen, setModalOpen] = useState(false);
+  const [confirmScheduleModalOpen, setConfirmScheduleModalOpen] = useState(false);
+  const [isScheduleConfirmed, setIsScheduleConfirmed] = useState(false);
 
   const {
     eventUserName,
@@ -52,160 +54,166 @@ function PageSchedule() {
 
     if (!isCorrect) return setInputWarningShouldAppear(true);
 
-    setModalOpen(true);
+    setConfirmScheduleModalOpen(true);
   };
 
   const handleConfirmSchedule = async () => {
-    setModalOpen(false);
+    setConfirmScheduleModalOpen(false);
+    setIsScheduleConfirmed(true);
     await whatsAppRequest(state);
   };
 
   const handleCloseModal = () => {
-    setModalOpen(false);
+    setConfirmScheduleModalOpen(false);
   };
 
   return (
-    <form className={ styles.form }>
-      <Modal
-        isOpen={ modalOpen }
-        onClose={ handleCloseModal }
-        onConfirm={ handleConfirmSchedule }
-      >
-        <h3>Confirmação de agendamento</h3>
-        <p>Deseja confirmar o agendamento de sua produção?</p>
-      </Modal>
-      <div className={ styles['form-group-1'] }>
-        <Input
-          label="Nome"
-          id="event-user-name"
-          type="text"
-          inputValue={ eventUserName }
-          placeHolder="Digite seu nome"
-          maxInputLength={ 32 }
-          name="eventUserName"
-          handleChange={ handleChange }
-          isInputCorrect={ isInputEmpty(eventUserName) && inputWarningShouldAppear }
-        />
-        <Input
-          label="Contato (WhatsApp)"
-          id="event-user-tel"
-          type="number"
-          inputValue={ eventUserTel }
-          placeHolder="Digite seu número de WhatsApp"
-          maxInputLength={ 12 }
-          name="eventUserTel"
-          handleChange={ handleChange }
-          isInputCorrect={ isInputEmpty(eventUserTel) && inputWarningShouldAppear }
-        />
-        <Select
-          id="production-type-select"
-          label="Selecione a sua produção"
-          options={
-            ['Maquiagem e penteado (R$ 180,00)',
-              'Apenas maquiagem (R$ 130,00) ',
-              'Apenas penteado (R$ 50,00)']
-          }
-          inputValue={ productionType }
-          name="productionType"
-          handleChange={ handleChange }
-        />
-        <Input
-          label="Evento"
-          id="event-input"
-          type="text"
-          inputValue={ eventName }
-          placeHolder="Casamento, formatura, ensaio fotográfico"
-          maxInputLength={ 32 }
-          name="eventName"
-          handleChange={ handleChange }
-          isInputCorrect={ isInputEmpty(eventName) && inputWarningShouldAppear }
-        />
-        <Input
-          label="Período do evento"
-          id="event-period-input"
-          type="text"
-          min="2023-07-23"
-          inputValue={ eventPeriod }
-          placeHolder="Manhã, tarde, noite"
-          maxInputLength={ 32 }
-          name="eventPeriod"
-          handleChange={ handleChange }
-          isInputCorrect={ isInputEmpty(eventPeriod) && inputWarningShouldAppear }
-        />
-        <label
-          className={ styles['date-label'] }
-          htmlFor="event-date-input"
-        >
-          Data do evento
-        </label>
-        <DatePicker
-          id="event-date-input"
-          selected={ eventDate }
-          onChange={ (date) => setState((prevState) => ({
-            ...prevState,
-            eventDate: date,
-          })) }
-          includeDates={ [new Date()] }
-          locale={ ptBR }
-          dateFormat="dd/MM/yyyy"
-          wrapperClassName={
-            isDateInputEmpty(eventDate) && inputWarningShouldAppear
-              ? styles['date-picker-wrong']
-              : styles['date-picker']
-          }
-          placeholderText="Escolha a data do evento"
-          disabled={ modalOpen }
-        />
-        <label
-          className={ styles['date-label'] }
-          htmlFor="event-hour-input"
-        >
-          Horário
-        </label>
-        <DatePicker
-          id="event-hour-input"
-          selected={ eventHour }
-          onChange={ (hour) => setState((prevState) => ({
-            ...prevState,
-            eventHour: hour,
-          })) }
-          locale={ ptBR }
-          showTimeSelect
-          showTimeSelectOnly
-          dateFormat="h:mm aa"
-          wrapperClassName={
-            isDateInputEmpty(eventHour) && inputWarningShouldAppear
-              ? styles['date-picker-wrong']
-              : styles['date-picker']
-          }
-          placeholderText="Escolha o horário do evento"
-          disabled={ modalOpen }
-        />
-        <span className={ styles['date-advice'] }>
-          Caso você não tenha encontrado a data desejada, clique
-          <span> </span>
-          <a
-            href="https://wa.me/5544991343233?text=Oi%20Isa,%20gostaria%20de%20marcar%20uma%20produção!"
-            target="_blank"
-            rel="noreferrer"
+    <>
+      { isScheduleConfirmed && <ScheduleConfirmed /> }
+      { !isScheduleConfirmed && (
+        <form className={ styles.form }>
+          <Modal
+            isOpen={ confirmScheduleModalOpen }
+            onClose={ handleCloseModal }
+            onConfirm={ handleConfirmSchedule }
           >
-            aqui
-          </a>
-          <span> </span>
-          para entrar em contato comigo.
-        </span>
-        { inputWarningShouldAppear && (
-          <span className={ styles['inputs-advice'] }>
-            Preencha os campos obrigatórios
-          </span>
-        ) }
-        <Button
-          type="button"
-          label="Agendar"
-          onClick={ handleSchedule }
-        />
-      </div>
-    </form>
+            <h3>Confirmação de agendamento</h3>
+            <p>Deseja confirmar o agendamento de sua produção?</p>
+          </Modal>
+          <div className={ styles['form-group-1'] }>
+            <Input
+              label="Nome"
+              id="event-user-name"
+              type="text"
+              inputValue={ eventUserName }
+              placeHolder="Digite seu nome"
+              maxInputLength={ 32 }
+              name="eventUserName"
+              handleChange={ handleChange }
+              isInputCorrect={ isInputEmpty(eventUserName) && inputWarningShouldAppear }
+            />
+            <Input
+              label="Contato (WhatsApp)"
+              id="event-user-tel"
+              type="number"
+              inputValue={ eventUserTel }
+              placeHolder="Digite seu número de WhatsApp"
+              maxInputLength={ 12 }
+              name="eventUserTel"
+              handleChange={ handleChange }
+              isInputCorrect={ isInputEmpty(eventUserTel) && inputWarningShouldAppear }
+            />
+            <Select
+              id="production-type-select"
+              label="Selecione a sua produção"
+              options={
+                ['Maquiagem e penteado (R$ 180,00)',
+                  'Apenas maquiagem (R$ 130,00) ',
+                  'Apenas penteado (R$ 50,00)']
+              }
+              inputValue={ productionType }
+              name="productionType"
+              handleChange={ handleChange }
+            />
+            <Input
+              label="Evento"
+              id="event-input"
+              type="text"
+              inputValue={ eventName }
+              placeHolder="Casamento, formatura, ensaio fotográfico"
+              maxInputLength={ 32 }
+              name="eventName"
+              handleChange={ handleChange }
+              isInputCorrect={ isInputEmpty(eventName) && inputWarningShouldAppear }
+            />
+            <Input
+              label="Período do evento"
+              id="event-period-input"
+              type="text"
+              min="2023-07-23"
+              inputValue={ eventPeriod }
+              placeHolder="Manhã, tarde, noite"
+              maxInputLength={ 32 }
+              name="eventPeriod"
+              handleChange={ handleChange }
+              isInputCorrect={ isInputEmpty(eventPeriod) && inputWarningShouldAppear }
+            />
+            <label
+              className={ styles['date-label'] }
+              htmlFor="event-date-input"
+            >
+              Data do evento
+            </label>
+            <DatePicker
+              id="event-date-input"
+              selected={ eventDate }
+              onChange={ (date) => setState((prevState) => ({
+                ...prevState,
+                eventDate: date,
+              })) }
+              includeDates={ [new Date()] }
+              locale={ ptBR }
+              dateFormat="dd/MM/yyyy"
+              wrapperClassName={
+                isDateInputEmpty(eventDate) && inputWarningShouldAppear
+                  ? styles['date-picker-wrong']
+                  : styles['date-picker']
+              }
+              placeholderText="Escolha a data do evento"
+              disabled={ confirmScheduleModalOpen }
+            />
+            <label
+              className={ styles['date-label'] }
+              htmlFor="event-hour-input"
+            >
+              Horário
+            </label>
+            <DatePicker
+              id="event-hour-input"
+              selected={ eventHour }
+              onChange={ (hour) => setState((prevState) => ({
+                ...prevState,
+                eventHour: hour,
+              })) }
+              locale={ ptBR }
+              showTimeSelect
+              showTimeSelectOnly
+              dateFormat="h:mm aa"
+              wrapperClassName={
+                isDateInputEmpty(eventHour) && inputWarningShouldAppear
+                  ? styles['date-picker-wrong']
+                  : styles['date-picker']
+              }
+              placeholderText="Escolha o horário do evento"
+              disabled={ confirmScheduleModalOpen }
+            />
+            <span className={ styles['date-advice'] }>
+              Caso você não tenha encontrado a data desejada, clique
+              <span> </span>
+              <a
+                href="https://wa.me/5544991343233?text=Oi%20Isa,%20gostaria%20de%20marcar%20uma%20produção!"
+                target="_blank"
+                rel="noreferrer"
+              >
+                aqui
+              </a>
+              <span> </span>
+              para entrar em contato comigo.
+            </span>
+            { inputWarningShouldAppear && (
+              <span className={ styles['inputs-advice'] }>
+                Preencha os campos obrigatórios
+              </span>
+            ) }
+            <Button
+              type="button"
+              label="Agendar"
+              onClick={ handleSchedule }
+            />
+          </div>
+        </form>
+      ) }
+    </>
   );
 }
 
