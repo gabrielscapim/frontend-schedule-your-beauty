f6dafe4437d3279aea72345f2358e75a0c61cd28
Gabriel Scapim <scapimgabriel@gmail.com>
2024-01-27 21:07:49 -0300

    Delete header on requests

--- src/services/apiRequest.js
+++ src/services/apiRequest.js
@@ -5,24 +5,19 @@ const URL = import.meta.env.VITE_API_URL;
 const apiRequest = async (method, endpoint, data = null) => {
   try {
     let response;
-    const config = {
-      headers: {
-        Authorization: '',
-      },
-    };
 
     switch (method.toLowerCase()) {
     case 'get':
-      response = await axios.get(`${URL}${endpoint}`, config);
+      response = await axios.get(`${URL}${endpoint}`);
       break;
     case 'post':
-      response = await axios.post(`${URL}${endpoint}`, data, config);
+      response = await axios.post(`${URL}${endpoint}`, data);
       break;
     case 'put':
-      response = await axios.put(`${URL}${endpoint}`, data, config);
+      response = await axios.put(`${URL}${endpoint}`, data);
       break;
     case 'delete':
-      response = await axios.delete(`${URL}${endpoint}`, config);
+      response = await axios.delete(`${URL}${endpoint}`);
       break;
     default:
       throw new Error(`Invalid HTTP method: ${method}`);
